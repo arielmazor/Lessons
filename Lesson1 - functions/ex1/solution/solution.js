90ab6f4484177cf04e49d5bc3779d2395cd9986d
arielmazor <ariel.mazor.mail@gmail.com>
2021-03-31 12:44:00 +0300

    ....

--- Lesson1 - functions/ex1/solution/solution.js
+++
@@ -1,3 +0,0 @@
-function changeColor(color) {
-    $(".btn").css('background', `${color}`);
-}
--- Lesson1 - functions/ex2/solution/solution.js
+++
@@ -1,4 +0,0 @@
-function consloeMultiply(num1, num2) {
-    var multiplyValue = num1 * num2;
-    console.log(multiplyValue);
-}
